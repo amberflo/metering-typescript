76428746b0ed66a81269bd7ea12c18811524bed4
filipelbc <filipelbc@gmail.com>
2022-05-05 15:01:23 -0300

    [chore] Add base client

---
+++ src/baseClient.ts
@@ -0,0 +1,82 @@
+import axios, { AxiosInstance } from "axios";
+import { amberfloBaseUrl, userAgent } from './model/constants';
+
+export default class BaseClient {
+    /**
+     * Base Amberflo API client. Abstracts away the actual API calls and
+     * logging.
+     */
+
+    apiKey: string;
+    axiosInstance: AxiosInstance;
+    signature: string;
+    debug: boolean;
+
+    /**
+     * Initialize a new `BaseClient`
+     * @param {string} apiKey
+     * @param {boolean} debug: Whether to issue debug level logs or not
+     * @param {string} name: Name of the client
+     */
+    constructor(apiKey: string, debug: boolean, name: string) {
+        this.signature = `[amberflo-metering ${name}]:`;
+        this.apiKey = apiKey;
+        this.debug = debug;
+        this.axiosInstance = axios.create({
+            baseURL: amberfloBaseUrl,
+            responseType: 'json',
+            headers: {
+                "X-API-KEY": this.apiKey,
+                "Content-Type": "application/json",
+                "User-Agent": userAgent,
+            },
+            timeout: 30000
+        });
+    }
+
+    private log(level: string, message: string, ...args: any[]) {
+        console.log(new Date(), this.signature, level, message, ...args);
+    }
+
+    log_info(message: string, ...args: any[]) {
+        this.log('INFO', message, ...args)
+    }
+
+    log_debug(message: string, ...args: any[]) {
+        if (this.debug) {
+            this.log('DEBUG', message, ...args)
+        }
+    }
+
+    log_error(message: string, ...args: any[]) {
+        this.log('ERROR', message, ...args)
+    }
+
+    async get<TResponse, TParams>(path: string, params: TParams): Promise<TResponse> {
+        const action = `GET ${path}`
+        try {
+            this.log_debug(action, params);
+            const response = await this.axiosInstance.get<TResponse>(path, { params });
+            this.log_info(action, response.status);
+            return response.data
+        }
+        catch (error) {
+            this.log_error(action, error);
+            throw new Error(`${action} failed: ${error}`);
+        }
+    }
+
+    async post<TResponse, TPayload>(path: string, payload: TPayload): Promise<TResponse> {
+        const action = `POST ${path}`
+        try {
+            this.log_debug(action, payload);
+            const response = await this.axiosInstance.post<TResponse>(path, payload);
+            this.log_info(action, response.status);
+            return response.data
+        }
+        catch (error) {
+            this.log_error(action, error);
+            throw new Error(`${action} failed: ${error}`);
+        }
+    }
+}
