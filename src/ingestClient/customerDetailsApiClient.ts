27b1d6938d83d1c1ae649718f0334110897390c3
filipelbc <filipelbc@gmail.com>
2022-05-06 16:53:34 -0300

    [feature] Improve customer API client

--- src/ingestClient/customerDetailsApiClient.ts
+++ src/ingestClient/customerDetailsApiClient.ts
@@ -1,73 +1,43 @@
-import axios, { AxiosInstance, AxiosRequestConfig } from 'axios';
-import axiosRetry from 'axios-retry';
-import * as Constants from '../model/constants';
-import { CustomerDetailsApiPayload } from '../model/customerApiPayload';
-import * as Errors from '../model/errors';
+import BaseClient from "../baseClient";
+import { CustomerDetailsApiPayload, ICustomerDetails } from '../model/customerApiPayload';
 
-export class CustomerDetailsApiClient {
-    axiosInstance: AxiosInstance;
-    signature: string;
-    apiKey: string;
-    debug: boolean;
+type NotFound = Record<string, never>
+
+export class CustomerDetailsApiClient extends BaseClient {
 
     constructor(apiKey: string, debug = false) {
-        this.apiKey = apiKey;
-        this.signature = '[amberflo-metering CustomerDetailsApiClient]:';
-        this.axiosInstance = axios.create();
-        this.debug = debug;
-        axiosRetry(this.axiosInstance, {
-            retries: 3,
-            retryDelay: axiosRetry.exponentialDelay
-        });
+        super(apiKey, debug, 'CustomerDetailsApiClient', true);
     }
 
-    async post(payload: CustomerDetailsApiPayload) {
-        if(this.debug){
-            console.log(new Date(), this.signature, 'calling CustomerDetails API', payload);
-        }
-
-        const configGet: AxiosRequestConfig = {
-            url: '/customer-details-endpoint/?customerId=' + payload.customerId,
-            method: 'get',
-            baseURL: Constants.amberfloBaseUrl,
-            headers: {
-                "X-API-KEY": this.apiKey,
-                "Content-Type": "application/json"
-            },
-            timeout: 30000
-        };
-
-        const resultGet = await this.axiosInstance.request(configGet);
-        const httpMethod = (Object.keys(resultGet.data).length > 0) ? 'put' : 'post';
-        if(this.debug){
-            console.log(new Date(), this.signature, 'http method is:', httpMethod);
-        }
-
-        const config: AxiosRequestConfig = {
-            url: '/customer-details-endpoint',
-            method: httpMethod,
-            baseURL: Constants.amberfloBaseUrl,
-            headers: {
-                "X-API-KEY": this.apiKey,
-                "Content-Type": "application/json"
-            },
-            timeout: 30000,
-            data: payload
-        };
-
-        return this.axiosInstance.request(config)
-            .then((response) => {
-                if(this.debug){
-                    console.log(new Date(), this.signature, "response from CustomerDetails API: ", response.status, response.data);
-                }
-                if (response.status >= 300) {
-                    console.log(`call to CustomerDetails API failed ${response.status}, ${response.data}`);
-                    throw new Error(`${Errors.CUSTOMER_DETAILS_API_ERROR} ${response.status}, ${response.data}`);
-                }
-            })
-            .catch((error) => {
-                console.log(`${new Date()} ${this.signature} call to CustomerDetails API failed ${error}`);
-                throw new Error(`${new Date()}  ${this.signature} ${Errors.CUSTOMER_DETAILS_API_ERROR} ${error}`);
-            });
+    async list(): Promise<ICustomerDetails[]> {
+        return await this.do_get<ICustomerDetails[]>('/customers');
     }
+
+    async get(customerId: string): Promise<ICustomerDetails | NotFound> {
+        return await this.do_get<ICustomerDetails | NotFound>('/customers', { customerId })
+    }
+
+    async add(payload: CustomerDetailsApiPayload, createInStripe = false): Promise<ICustomerDetails> {
+        payload.validate()
+        const params = createInStripe ? { autoCreateCustomerInStripe: true } : undefined
+        return await this.do_post<ICustomerDetails>('/customers', payload, params)
+    }
+
+    async update(payload: CustomerDetailsApiPayload): Promise<ICustomerDetails> {
+        payload.validate()
+        return await this.do_put<ICustomerDetails>('/customers', payload)
+    }
+
+    async add_or_update(payload: CustomerDetailsApiPayload): Promise<ICustomerDetails> {
+        payload.validate()
+        const customer = await this.get(payload.customerId)
+        if (customer.id) {
+            return await this.update(payload)
+        } else {
+            return await this.add(payload)
+        }
+    }
+
+    // for backwards compatibility
+    post = this.add_or_update
 }
