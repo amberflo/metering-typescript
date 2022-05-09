751439acfa427d73b49e45eded9417f00003991d
filipelbc <filipelbc@gmail.com>
2022-05-09 11:13:55 -0300

    [docs] Add comments on API clients

--- src/usageClient.ts
+++ src/usageClient.ts
@@ -1,12 +1,14 @@
 import BaseClient from "./baseClient";
 import { UsageApiPayload, AllUsageApiPayload, IUsageReport } from "./model/usageApiPayload";
 
+/**
+ * See: https://docs.amberflo.io/reference/post_usage
+ */
 export class UsageClient extends BaseClient {
 
     /**
      * Initialize a new `UsageClient`
-     * @param {string} apiKey
-     * @param {boolean} debug: Whether to issue debug level logs or not
+     * `debug`: Whether to issue debug level logs or not.
      */
     constructor(apiKey: string, debug = false) {
         super(apiKey, debug, 'UsageClient');
@@ -14,8 +16,7 @@ export class UsageClient extends BaseClient {
 
     /**
      * Get usage data
-     * @param {UsageApiPayload} query
-     * @returns {Promise<UsageApiResult>}
+     * See: https://docs.amberflo.io/reference/post_usage
      */
     async getUsage(query: UsageApiPayload): Promise<IUsageReport> {
         query.validate();
@@ -23,9 +24,8 @@ export class UsageClient extends BaseClient {
     }
 
     /**
-     * Get usage data, in batch
-     * @param {UsageApiPayload[]} query
-     * @returns {Promise<UsageApiResult[]>}
+     * Get usage data, multiple reports at a time.
+     * See: https://docs.amberflo.io/reference/post_usage-batch
      */
     async getUsageBatch(queries: UsageApiPayload[]): Promise<IUsageReport[]> {
         queries.forEach((q) => q.validate());
@@ -33,9 +33,7 @@ export class UsageClient extends BaseClient {
     }
 
     /**
-     * Get usage data for all meters
-     * @param {AllUsageApiPayload} query
-     * @returns {Promise<UsageApiResult[]>}
+     * Get usage reports for all meters. Because it incudes all meters, this is more limited than `getUsage`.
      */
     async getAllUsage(query: AllUsageApiPayload): Promise<IUsageReport[]> {
         query.validate();
