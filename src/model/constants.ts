2c1e3adb8d00eacefea1f01a03cf0822ec66b60f
Filipe Correia <filipe@amberflo.io>
2023-01-10 14:50:40 -0300

    v2.5.2

--- src/model/constants.ts
+++ src/model/constants.ts
@@ -1,6 +1,6 @@
 export const amberfloBaseUrl = 'https://app.amberflo.io';
 
-const libVersion = "2.5.1";
+const libVersion = "2.5.2";
 
 export const userAgent = `Amberflo.io SDK ${libVersion}; Typescript; Node ${process.version}`;
 
